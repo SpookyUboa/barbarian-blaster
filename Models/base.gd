5945213667aee2202092346356320a2a41264c4a
Edu Romero <eduardojrl927@gmail.com>
2024-08-28 21:51:57 +0200

    Added health label and collision with enemy

---
+++ Models/base.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@export var max_health : int = 10
+@onready var health_label: Label3D = $HealthLabel
+
+var current_health : int
+
+func _ready() -> void:
+	current_health = max_health
+	health_label.text = str(current_health)
+
+func take_damage():
+	current_health -= 1
+	health_label.text = str(current_health)
+	print("TOOK DAMAGE!!")
+	
+
+
+func _on_body_entered(body: Node) -> void:
+	print("COLLISION!!!")
+	if "enemy" in body.get_groups():
+		take_damage()
+	else:
+		print("NOT AN ENEMY?!")
--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -2,6 +2,10 @@ extends PathFollow3D
 
 @export var speed = 1
 
+@onready var base = get_tree().get_first_node_in_group("base")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	progress += delta * speed
+	progress += delta * speed * 5
+	if progress_ratio == 1.0 : 
+		base.take_damage()
