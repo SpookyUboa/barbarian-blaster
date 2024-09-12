603bc1678339d2dc6cd12d2eae2c983a3a5b1c00
Edu Romero <eduardojrl927@gmail.com>
2024-09-12 19:56:11 +0200

    Colored health label and raycast picker

--- Models/base.gd
+++ Models/base.gd
@@ -3,16 +3,23 @@ extends Node3D
 @export var max_health : int = 10
 @onready var health_label: Label3D = $HealthLabel
 
-var current_health : int
+var current_health : int:
+	set(health_in):
+		current_health = health_in
+		health_label.text = str(current_health) + "/" + str(max_health)
+		var red: Color = Color.RED
+		var white: Color = Color.WHITE
+		var blended_color = red.lerp(white,float(current_health) / float(max_health))
+		health_label.modulate = blended_color
+		if current_health < 1:
+			get_tree().reload_current_scene()
 
 func _ready() -> void:
 	current_health = max_health
-	health_label.text = str(current_health)
+	health_label.text = str(current_health) + "/" + str(max_health)
 
 func take_damage():
 	current_health -= 1
-	health_label.text = str(current_health)
-	print("TOOK DAMAGE!!")
 	
 
 
