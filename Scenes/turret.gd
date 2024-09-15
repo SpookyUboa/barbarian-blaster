9312f44d3655cc9090ffa84d026552c1dd944528
Edu Romero <eduardojrl927@gmail.com>
2024-09-15 19:56:23 +0200

    Firing timer

--- Scenes/turret.gd
+++ Scenes/turret.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
 @export var projectile: PackedScene
+@onready var cannon: MeshInstance3D = $Cannon
 
-func _ready() -> void:
+func _on_timer_timeout() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = cannon.global_position
