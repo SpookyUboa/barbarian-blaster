f8c2fd0fc2e3abdb4f50faf9f8f6f298612ed5dc
Edu Romero <eduardojrl927@gmail.com>
2024-10-13 21:58:45 +0200

    Retry/Quit options on defeat. Implementation can very likely be improved

--- Models/base.gd
+++ Models/base.gd
@@ -1,18 +1,21 @@
 extends Node3D
 
 @export var max_health : int = 10
+@export var defeat_layer : CanvasLayer
+@export var defeat_message : Label
 @onready var health_label: Label3D = $HealthLabel
 
 var current_health : int:
 	set(health_in):
-		current_health = health_in
+		current_health = max(0, health_in)
 		health_label.text = str(current_health) + "/" + str(max_health)
 		var red: Color = Color.RED
 		var white: Color = Color.WHITE
 		var blended_color = red.lerp(white,float(current_health) / float(max_health))
 		health_label.modulate = blended_color
 		if current_health < 1:
-			get_tree().reload_current_scene()
+			defeat_message.text = "DEFEAT"
+			defeat_layer.show()
 
 func _ready() -> void:
 	current_health = max_health
